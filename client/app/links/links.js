2adca6bf53a9ab4c3f22830541c2217b06957ddc
taylorleh <taylorleh@mac.com>
2015-09-19 10:56:48 -0700

    Adds routes in app.js and sets up model in shorten

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,14 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+
+  $scope.data = {};
+
+  Links.getLinks();
+
+  $scope.getLinks = function () {
+    Links.getLinks();
+  };
+
+  $scope.getLinks();
 });
